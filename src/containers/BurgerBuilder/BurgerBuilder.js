ffb49dc860f300a629acce0635e5af8de6ce5578
iDmitriy-dev <dmitriy.p86@ya.ru>
2019-12-15 23:43:52 +0600

    fetching data from server with redux-thunk

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,9 +7,8 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../store/actions/index';
 import axios from '../../axios-orders';
-// import ENV from '../../env';
-import * as actionTypes from '../../store/actions';
 
 
 class BurgerBuilder extends Component {
@@ -18,20 +17,11 @@ class BurgerBuilder extends Component {
    //     this.state = { ...}
    // }
    state = {
-      purchasing: false,
-      loading: false,
-      error: false
+      purchasing: false
    }
 
    componentDidMount() {
-      // axios.get(`${ENV.firebaseBaseURL}/ingredients.json`)
-      //    .then(response => {
-      //       this.setState({ ingredients: response.data })
-      //       //console.log(this.state.ingredients)
-      //    })
-      //    .catch(error => {
-      //       this.setState({ error: true })
-      //    });
+      this.props.onInitIngredients()
    }
 
    updatePurchaseState(ingredients) {
@@ -85,6 +75,7 @@ class BurgerBuilder extends Component {
    }
 
    purchaseContinueHandler = () => {
+      this.props.onInitPurchase()
       this.props.history.push('/checkout');
    }
 
@@ -96,7 +87,7 @@ class BurgerBuilder extends Component {
          disabledInfo[key] = disabledInfo[key] <= 0
       }
       let orderSummary = null;
-      let burger = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner />
+      let burger = this.props.error ? <p>Ingredients can't be loaded!</p> : <Spinner />
 
       if (this.props.ings) {
          burger = (
@@ -118,9 +109,9 @@ class BurgerBuilder extends Component {
             purchaseContinued={this.purchaseContinueHandler}
          />;
       }
-      if (this.state.loading) {
-         orderSummary = <Spinner />;
-      }
+      // if (this.state.loading) {
+      //    orderSummary = <Spinner />;
+      // }
       return (
          <>
             <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
@@ -134,15 +125,18 @@ class BurgerBuilder extends Component {
 
 const mapStateToProps = state => {
    return {
-      ings: state.ingredients,
-      price: state.totalPrice
+      ings: state.burgerBuilder.ingredients,
+      price: state.burgerBuilder.totalPrice,
+      error: state.burgerBuilder.error
    }
 }
 
 const mapDispatchToProps = dispatch => {
    return {
-      onIngredientAdded: (ingName) => dispatch({ type: actionTypes.ADD_INGREDIENT, ingredientName: ingName }),
-      onIngredientRemoved: (ingName) => dispatch({ type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName })
+      onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
+      onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
+      onInitIngredients: () => dispatch(actions.initIngredients()),
+      onInitPurchase: () => dispatch(actions.purchaseInit())
    }
 }
 
---
+++ src/store/actions/order.js
@@ -0,0 +1,83 @@
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+   return {
+      type: actionTypes.PURCHASE_BURGER_SUCCESS,
+      orderId: id,
+      orderData: orderData
+   }
+};
+
+export const purchaseBurgerFail = (error) => {
+   return {
+      type: actionTypes.PURCHASE_BURGER_FAIL,
+      error: error
+   }
+};
+
+export const purchaseBurgerStart = () => {
+   return {
+      type: actionTypes.PURCHASE_BURGER_START
+   }
+}
+
+export const purchaseBurger = (orderData) => {
+   return dispatch => {
+      dispatch(purchaseBurgerStart());
+      axios.post('/orders.json', orderData)
+         .then(response => {
+            //console.log(response.data, orderData)
+            dispatch(purchaseBurgerSuccess(response.data.name, orderData))
+         })
+         .catch(error => {
+            dispatch(purchaseBurgerFail(error))
+         })
+   }
+};
+
+export const purchaseInit = () => {
+   return {
+      type: actionTypes.PURCHASE_INIT
+   }
+};
+
+export const fetchordersSuccess = (orders) => {
+   return {
+      type: actionTypes.FETCH_ORDERS_SUCCESS,
+      orders: orders
+   }
+};
+
+export const fetchordersFail = (error) => {
+   return {
+      type: actionTypes.FETCH_ORDERS_FAIL,
+      error: error
+   }
+};
+
+export const fetchordersStart = () => {
+   return {
+      type: actionTypes.FETCH_ORDERS_START
+   }
+};
+
+export const fetchOrders = () => {
+   return dispatch => {
+      dispatch(fetchordersStart());
+      axios.get('/orders.json')
+      .then(res => {
+         const fetchedOrders = [];
+         for (let key in res.data) {
+            fetchedOrders.push({
+               ...res.data[key],
+               id: key
+            });
+         }
+         dispatch(fetchordersSuccess(fetchedOrders));
+      })
+      .catch(err => {
+         dispatch(fetchordersFail(err))
+      })
+   }
+};
